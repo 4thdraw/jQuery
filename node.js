7e9c7784d63bc949e9a36c98e321115d53fc2bde
4thdraw <4thdraw2@gmail.com>
2023-05-15 17:59:17 +0900

    node.js for문공부

--- node.js
+++ node.js
@@ -6,9 +6,17 @@ var number = 1;
 console.log(myname)
 console.log(typeof myname)
 
-document.body.innerHTML = "<h2>";
-document.body.innerHTML += myname
-document.body.innerHTML +="</h2>"
-document.body.innerHTML +="<p>자바스크립트 "
-document.body.innerHTML += number+"일 공부시작</p>";
+// document.body.innerHTML = "<h2>";
+// document.body.innerHTML += myname
+// document.body.innerHTML +="</h2>"
+// document.body.innerHTML +="<p>자바스크립트 "
+// document.body.innerHTML += number+"일 공부시작</p>";
+
+
+var count = 0;
+
+for(var i = 0; i < 100; i+=1 ){
+    count += 1; 
+}
+console.log("count 변수의 값은?",count)
 
