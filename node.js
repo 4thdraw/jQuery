a0f9bf2bd08c7239c71dc78a08b36766e4fec575
4thdraw <4thdraw2@gmail.com>
2023-05-15 15:51:58 +0900

    선택자 / 메서드 / 이벤트

---
+++ node.js
@@ -0,0 +1,14 @@
+var myname = "방보영"
+var number = 1; 
+    number += 10; 
+    number -= 5;
+
+console.log(myname)
+console.log(typeof myname)
+
+document.body.innerHTML = "<h2>";
+document.body.innerHTML += myname
+document.body.innerHTML +="</h2>"
+document.body.innerHTML +="<p>자바스크립트 "
+document.body.innerHTML += number+"일 공부시작</p>";
+
